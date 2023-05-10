6f23fa8a60e42fe738ac19756a2bb6568f221883
toniebe <toniebe48@gmail.com>
2023-05-10 23:19:00 +0700

    feat[screen]: create booking container and presentation

--- src/core/container/BookingContainer.tsx
+++ src/core/container/BookingContainer.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
-import BookingPresentation from '../presentation/BookingPresentation';
+/* eslint-disable react-hooks/exhaustive-deps */
+import BookingPresentation from '@presentation/BookingPresentation';
+import React, {useEffect} from 'react';
+import {useSelector} from 'react-redux';
 
 const BookingContainer = () => {
+  const dataBooking = useSelector((state: any) => state.hotel);
+  useEffect(() => {
+    console.log(dataBooking);
+  }, []);
   return <BookingPresentation />;
 };
 
--- src/core/presentation/BookingPresentation.tsx
+++ src/core/presentation/BookingPresentation.tsx
@@ -1,14 +1,22 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import LayoutPlatform from '@sharedComponents/organisms/LayoutPlatform';
 
 const BookingPresentation = () => {
   return (
-    <View style={styles}>
-      <Text>BookingPresentation</Text>
-    </View>
+    <LayoutPlatform saveArea={false} style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text>BookingPresentation</Text>
+      </View>
+    </LayoutPlatform>
   );
 };
 
 export default BookingPresentation;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
