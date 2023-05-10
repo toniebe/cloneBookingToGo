03dee8ee5abd8f405d3a25d17a765020efbfc7f4
toniebe <toniebe48@gmail.com>
2023-05-10 23:19:28 +0700

    feat[screen]: create addguest container and presentation

---
+++ src/core/container/AddGuestContainer.tsx
@@ -0,0 +1,8 @@
+import AddGuestPresentation from '@presentation/AddGuestPresentation';
+import React from 'react';
+
+const AddGuestContainer = () => {
+  return <AddGuestPresentation />;
+};
+
+export default AddGuestContainer;
---
+++ src/core/presentation/AddGuestPresentation.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const AddGuestPresentation = () => {
+  return (
+    <View>
+      <Text>AddGuestPresentation</Text>
+    </View>
+  );
+};
+
+export default AddGuestPresentation;
+
+const styles = StyleSheet.create({});
