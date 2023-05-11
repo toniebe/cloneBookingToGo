3e02e5695cdc2ec017180e89faf17f406b3088fe
toniebe <toniebe48@gmail.com>
2023-05-11 19:39:15 +0700

    fix[integration]: integration add guest with reducer

--- src/core/presentation/AddGuestPresentation.tsx
+++ src/core/presentation/AddGuestPresentation.tsx
@@ -1,14 +1,65 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
+import CardEditGuest from '../components/atomics/CardEditGuest';
+import {ListGuest} from '../store/reducer/guestReducer';
+import {scale, verticalScale} from '@sharedHelper/scaling';
 
-const AddGuestPresentation = () => {
+interface AddGuestProps {
+  listGuest: ListGuest[];
+  handleAddGuest: () => void;
+  handleValueNameChange: (value: string, index: number) => void;
+  handleTitleNameChange: (value: string, index: number) => void;
+  setValue: any;
+  actionDelete: (guest: ListGuest) => void;
+}
+
+const AddGuestPresentation = ({
+  listGuest,
+  handleAddGuest,
+  handleValueNameChange,
+  handleTitleNameChange,
+  setValue,
+  actionDelete,
+}: AddGuestProps) => {
   return (
-    <View>
-      <Text>AddGuestPresentation</Text>
-    </View>
+    <ScrollView nestedScrollEnabled>
+      {listGuest.map((guest, index) => (
+        <View
+          key={index}
+          style={{marginVertical: scale(10), zIndex: listGuest.length - index}}>
+          <CardEditGuest
+            key={index}
+            setValue={setValue}
+            valueDropdown={guest.titleName}
+            onSelectItem={item => handleTitleNameChange(item.value, index)}
+            valueName={guest.name}
+            onChangeValueName={(value: any) => {
+              handleValueNameChange(value, index);
+            }}
+            actionDelete={() => actionDelete(guest)}
+          />
+        </View>
+      ))}
+      <TouchableOpacity style={styles.addContainer} onPress={handleAddGuest}>
+        <Text>+ Tambah Data Tamu</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
 export default AddGuestPresentation;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  addContainer: {
+    padding: scale(15),
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: verticalScale(100),
+  },
+});
